d92a203de0bf39e92df26b1e1bd312b530d2d72d
Jet Balagtas <jetbalagtas@gmail.com>
2019-03-10 18:10:44 -0400

    practice data-binding

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  username = '';
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  onResetUsername(event: Event) {
+    this.username = '';
+  }
+}
